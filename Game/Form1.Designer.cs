d8e0d66b728354517697a5c5cb35a8ed2c6d3518
Poln fuel <polnfuel@gmail.com>
2024-02-28 22:29:16 +0200

    Лабораторная №6

--- Game/Form1.Designer.cs
+++ Game/Form1.Designer.cs
@@ -28,19 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            WordList = new ListBox();
+            RunButton = new Button();
+            TitleLabel = new Label();
+            ResultLabel = new Label();
             SuspendLayout();
+            // 
+            // WordList
+            // 
+            WordList.FormattingEnabled = true;
+            WordList.Items.AddRange(new object[] { "Обычное предложение", "Тут  между    словами  несколько    пробелов!", "Слово", "Два слова", "Это самая длинная строка в этом списке - в ней 12 слов", " ", "В предыдущей строке стоит пробел." });
+            WordList.Location = new Point(104, 69);
+            WordList.Name = "WordList";
+            WordList.Size = new Size(446, 264);
+            WordList.TabIndex = 0;
+            // 
+            // RunButton
+            // 
+            RunButton.Location = new Point(138, 350);
+            RunButton.Name = "RunButton";
+            RunButton.Size = new Size(140, 40);
+            RunButton.TabIndex = 1;
+            RunButton.Text = "Считать слова";
+            RunButton.UseVisualStyleBackColor = true;
+            RunButton.Click += RunButton_Click;
+            // 
+            // TitleLabel
+            // 
+            TitleLabel.AutoSize = true;
+            TitleLabel.Font = new Font("Segoe UI", 10F);
+            TitleLabel.Location = new Point(46, 9);
+            TitleLabel.Name = "TitleLabel";
+            TitleLabel.Size = new Size(602, 46);
+            TitleLabel.TabIndex = 2;
+            TitleLabel.Text = "Подсчитаем количество слов в выбранной строке\r\n(считается что слово - любой набор символов ограниченный пробелами)";
+            // 
+            // ResultLabel
+            // 
+            ResultLabel.AutoSize = true;
+            ResultLabel.Location = new Point(342, 360);
+            ResultLabel.Name = "ResultLabel";
+            ResultLabel.Size = new Size(85, 20);
+            ResultLabel.TabIndex = 3;
+            ResultLabel.Text = "ResultLabel";
             // 
             // Window
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(682, 453);
+            Controls.Add(ResultLabel);
+            Controls.Add(TitleLabel);
+            Controls.Add(RunButton);
+            Controls.Add(WordList);
             Name = "Window";
             Text = "Game";
-            MouseClick += Window_MouseClick;
+            Load += Window_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox WordList;
+        private Button RunButton;
+        private Label TitleLabel;
+        private Label ResultLabel;
     }
 }
--- Game/Form1.cs
+++ Game/Form1.cs
@@ -6,26 +6,42 @@ namespace Game
         {
             InitializeComponent();
         }
-
-        private void Window_MouseClick(object sender, MouseEventArgs e)
+        private void Window_Load(object sender, EventArgs e)
         {
-            Button b = new Button();
-            TextBox t = new TextBox();
-            if (e.X < 350)
-            {
-                b.Parent = this;
-                b.Location = new Point(e.X, e.Y);
-                b.Size = new Size(40, 40);
-                b.Text = "BB";
-            }
-            else if (e.X > 350)
-            {
-                t.Parent = this;
-                t.Location = new Point(e.X, e.Y);
-                t.Size = new Size(40, 40);
-                t.Text = "TT";
-            }
-
+            ResultLabel.Text = string.Empty;
         }
+        private void RunButton_Click(object sender, EventArgs e)
+        {
+            int StringIndex = WordList.SelectedIndex;
+            string SelectedString = (string)WordList.Items[StringIndex];
+            int StringLength = SelectedString.Length;
+            int Words = 0;
+            int i = 0;
+            char Prev = ' ';
+            char Curr = SelectedString[0];
+            while (i < StringLength)
+            {
+                if (Curr != ' ' && Prev == ' ')
+                    Words++;
+                Prev = SelectedString[i];
+                if (i != StringLength - 1)
+                    Curr = SelectedString[i + 1];
+                i++;
+            }
+            ResultLabel.Text = "В этой строке " + Words.ToString();
+            switch (Words)
+            {
+                case 1:
+                    ResultLabel.Text += " слово"; break;
+                case 2:
+                case 3:
+                case 4:
+                    ResultLabel.Text += " слова"; break;
+                default:
+                    ResultLabel.Text += " слов"; break;
+            }
+        }
+
+        
     }
 }
